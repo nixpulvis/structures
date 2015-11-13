b785648a862d9619d5b19a67339f838d3284c218
Nathan Lilienthal <nathan@nixpulvis.com>
2015-11-13 01:48:41 -0500

    Small docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,10 @@
+//! Data structures of various shapes and sizes. These structures are not
+//! necessarily highly optimized or intended for production scale use. They
+//! are however useful for learning about Rust, and how the core building
+//! blocks of the language work.
+//!
+//! Along that line, generally in cases where there are trade offs between
+//! design clarity and performance, this crate chooses design clarity.
+
 pub mod list;
 pub mod tree;
--- src/tree/mod.rs
+++ src/tree/mod.rs
@@ -1,3 +1,5 @@
+//! Trees are non-cyclic graphs with a singular root node.
+
 pub use tree::binary_tree::{BinaryTree};
 
 pub mod binary_tree;
